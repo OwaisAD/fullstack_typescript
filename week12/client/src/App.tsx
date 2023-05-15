fbb6d35a0c8b82fe1ca168203e735beb4eda4230
Owais <33609590+OwaisAD@users.noreply.github.com>
2023-05-15 15:02:33 +0200

    week12

--- week12/client/src/App.tsx
+++ week12/client/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { toast } from "react-hot-toast";
+import { setContext } from "@apollo/client/link/context";
+import { Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import Error from "./page/Error";
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) {
+    if (graphQLErrors[0].message === "Cannot read properties of undefined (reading 'thread_id')")
+      return;
+    graphQLErrors.map(({ message }) => {
+      toast.error(`${message}`);
+    });
+  }
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("ecoflipr-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = from([errorLink, new HttpLink({ uri: "http://localhost:3001/graphql" })]);
+
+const client = new ApolloClient({
+  link: authLink.concat(link),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ApolloProvider client={client}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
